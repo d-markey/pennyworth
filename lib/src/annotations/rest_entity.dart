f65b5c825f7d11b602fbbb57ad87f0cedc439a4c
d-markey <1548408+d-markey@users.noreply.github.com>
2022-01-01 19:14:10 +0100

    Annotation parameter to enable/disable code generation for de/serialization methods

--- lib/src/annotations/rest_entity.dart
+++ lib/src/annotations/rest_entity.dart
@@ -2,11 +2,20 @@
 /// Several annotations may be used together to fully qualify the REST entity.
 class RestEntity {
   /// General purpose constructor.
-  const RestEntity({this.title, this.tags});
+  const RestEntity({this.title, this.tags, this.autoSerialize});
+
+  /// Constant to disable code generation for auto-de/serialization methods
+  static const noAutoSerialization = RestEntity(autoSerialize: false);
+
+  /// Constant to force code generation for auto-de/serialization methods
+  static const autoSerialization = RestEntity(autoSerialize: true);
 
   /// A description for the REST entity
   final String? title;
 
   /// Tags associated with the REST entity
   final List<String>? tags;
+
+  /// Whether Pennyworth should generate JSON de/serialization methods
+  final bool? autoSerialize;
 }
